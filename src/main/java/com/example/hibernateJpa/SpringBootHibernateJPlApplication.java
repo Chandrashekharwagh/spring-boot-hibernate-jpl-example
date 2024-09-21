829730e8c1c4466f1157a34e64454dd216d8512e
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-09-21 10:44:59 +0530

    commits

---
+++ src/main/java/com/example/hibernateJpa/SpringBootHibernateJPlApplication.java
@@ -0,0 +1,11 @@
+package com.example.hibernateJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHibernateJPlApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHibernateJPlApplication.class, args);
+    }
+}
